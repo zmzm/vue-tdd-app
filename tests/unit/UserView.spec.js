ffaa2abf6b2fccbe4c5ac7594be47bffc33229cc
Uladzislau Kavalski <vladislav.kovalskij@gmail.com>
2022-06-28 00:10:55 +0200

    tests refactoring

--- tests/unit/UserView.spec.js
+++ tests/unit/UserView.spec.js
@@ -1,26 +1,33 @@
 import { shallowMount } from '@vue/test-utils';
 
 import UserView from '@/views/UserView';
-import UserSearchForm from '@/components/UserSearchForm'
-import UserProfile from '@/components/UserProfile'
+import UserSearchForm from '@/components/UserSearchForm';
+import UserProfile from '@/components/UserProfile';
 
 describe('UserView', () => {
+  const build = () => {
+    const wrapper = shallowMount(UserView);
+
+    return {
+      wrapper,
+      userSearchForm: () => wrapper.findComponent(UserSearchForm),
+      userProfile: () => wrapper.findComponent(UserProfile),
+    };
+  };
+
   it('renders the component', () => {
     // arrange
-    const wrapper = shallowMount(UserView)
-
+    const { wrapper } = build();
     // assert
-    expect(wrapper.html()).toMatchSnapshot()
-  })
+    expect(wrapper.html()).toMatchSnapshot();
+  });
 
   it('renders main child components', () => {
     // arrange
-    const wrapper = shallowMount(UserView)
-    const userSearchForm = wrapper.findComponent(UserSearchForm)
-    const userProfile = wrapper.findComponent(UserProfile)
+    const { userSearchForm, userProfile } = build();
 
     // assert
-    expect(userSearchForm.exists()).toBe(true)
-    expect(userProfile.exists()).toBe(true)
-  })
+    expect(userSearchForm().exists()).toBe(true);
+    expect(userProfile().exists()).toBe(true);
+  });
 });
