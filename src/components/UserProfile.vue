e4a21ef4dd7b425d245195d8eb712a1a4f40beea
Uladzislau Kavalski <vladislav.kovalskij@gmail.com>
2022-07-11 22:56:11 +0200

    added user profile tests

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -12,5 +12,13 @@ export default {
 </script>
 
 <template>
-  <div>UserProfile</div>
+  <div class="user-profile">
+    <img class="user-profile__avatar" :src="user.avatar_url" />
+    <div class="user-profile__name">
+      {{ user.name }}
+    </div>
+    <div class="user-profile__bio">
+      {{ user.bio }}
+    </div>
+  </div>
 </template>
---
+++ tests/unit/UserProfile.spec.js
@@ -0,0 +1,48 @@
+import { shallowMount } from '@vue/test-utils';
+
+import UserProfile from '@/components/UserProfile';
+
+import userFixture from '../fixtures/userFixture';
+
+describe('UserProfile', () => {
+  let props = {};
+
+  const build = () => {
+    const wrapper = shallowMount(UserProfile, {
+      props,
+    });
+
+    return {
+      wrapper,
+      avatar: () => wrapper.find('.user-profile__avatar'),
+      name: () => wrapper.find('.user-profile__name'),
+      bio: () => wrapper.find('.user-profile__bio'),
+    };
+  };
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+    props = { user: userFixture };
+  });
+
+  it('renders the component', () => {
+    // arrange
+    const { wrapper } = build();
+    // assert
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('renders main components', () => {
+    // arrange
+    const { avatar, name, bio } = build();
+    // assert
+    expect(avatar().exists()).toBe(true);
+    expect(avatar().attributes().src).toBe(props.user.avatar_url);
+
+    expect(name().exists()).toBe(true);
+    expect(name().text()).toBe(props.user.name);
+
+    expect(bio().exists()).toBe(true);
+    expect(bio().text()).toBe(props.user.bio);
+  });
+});
